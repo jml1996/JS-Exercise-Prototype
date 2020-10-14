802ce4c2c24cfe33966443446e7d73531a4b89b3
Joshua Lovins <joshualovins@joshuas-mbp.home>
2020-10-14 17:40:11 -0400

    Finished MVP, all tests passing. May return for Stretch later tonight.

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(nm, ag) {
+  this.name = nm;
+  this.age = ag;
+  this.stomach = [];
 }
+Person.prototype.eat = function (food) {
+  if (this.stomach.length < 10){
+    this.stomach.push(food);
+  }
+}
+Person.prototype.poop = function () {
+  this.stomach = [];
+}
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+}
+
+// let a = new Person("Mary", 50);
+// console.log(a.toString());
 
 /*
   TASK 2
@@ -57,8 +73,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(mdl, mpg) {
+  this.model = mdl;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function (gallons){
+  this.tank += gallons;
 }
 
 /*
@@ -68,18 +90,34 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+// function Baby(favToy) {
+//   Person.call(this, "Baby", "Younger than 3");
+//   this.favoriteToy = favToy;
+// }
+// Baby.prototype = Object.create(Person.prototype);
+// Baby.prototype.play = function (){
+//   return `Playing with ${this.favoriteToy}`;
+// }
+
+function Baby(nm, ag, favToy) {
+  this.name = nm;
+  this.age = ag;
+  this.favoriteToy = favToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function (){
+    return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. This refers to the object in which it is used.
+  2. If this is used in a function defined in an object, it refers to the object of which the function is a value.
+  3. If this is used in a constructor function, it sets the key for the constructor function.
+  4. If this is used in an ordinary function not defined as an object's value, it must be bound explicitly to an object, or else it will return undefined.
 */
 
 
